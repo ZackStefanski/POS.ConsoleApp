21da9701bccc8a8c5646da9eaa23a037c000bde1
ZackStefanski <97365684+ZackStefanski@users.noreply.github.com>
2022-01-25 10:29:44 -0500

    began work on mm

--- POS.ConsoleApp/POS.ConsoleApp/Program.cs
+++ POS.ConsoleApp/POS.ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zack's POS Console App");
+﻿using System;
+
+namespace POS
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("**** Point of Sale ****");
+            Console.WriteLine("***********************");
+            Console.WriteLine("");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1: Rent Equipment");
+            Console.WriteLine("2: Create New Customer");
+            Console.WriteLine("3: View/Edit Customers");
+            int userInput = Console.ReadLine();
+            if (c)
+
+            Console.ReadLine();
+        }
+    }
+}
